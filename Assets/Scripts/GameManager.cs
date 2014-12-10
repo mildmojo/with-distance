c481708efd9f13e89e8f5f0bde9e4b3920901d7a
mildmojo <theaterfire@yahoo.com>
2014-12-10 03:10:55 -0500

    Adds project files.
    Currently uses a rolling average of 10 distance sensor readings to
    adjust camera position, moving through story cards. If sensor isn't
    attached, uses arrow keys.
    
    Still to do: try a second sensor for added accuracy, maybe add
    knobs or switches for calibration. Write actual story. Add attract
    mode, multi-narrative switching.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+// using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class GameManager : MonoBehaviour {
+
+  public static GameManager Instance;
+
+  public GameObject BillboardPrefab;
+  public float XSpacing;
+  public float ZSpacing;
+  public List<TextAsset> StoryFiles;
+
+  [HideInInspector] [System.NonSerialized]
+  public int BillboardIdx;
+
+  [HideInInspector] [System.NonSerialized]
+  public List<int> StoryCounts;
+  [HideInInspector] [System.NonSerialized]
+  public List<GameObject> Billboards;
+  [HideInInspector] [System.NonSerialized]
+  public int StoryIdx;
+
+  void Awake() {
+    Instance = Instance ?? this;
+  }
+
+  // Use this for initialization
+  void Start () {
+    BillboardIdx = 0;
+    StoryIdx = 0;
+    LoadStoryFiles();
+  }
+
+  // Update is called once per frame
+  void Update () {
+    if (Input.GetKeyDown(KeyCode.DownArrow)) {
+      BillboardIdx++;
+    } else if (Input.GetKeyDown(KeyCode.UpArrow)) {
+      BillboardIdx--;
+    }
+
+    BillboardIdx = Mathf.Clamp(BillboardIdx, 0, StoryCounts[StoryIdx] - 1);
+  }
+
+  void LoadStoryFiles() {
+    StoryCounts = new List<int>();
+    Billboards = new List<GameObject>();
+    var pos = Vector3.zero;
+
+    foreach (var file in StoryFiles) {
+      var lines = file.text.Split('\n').ToList();
+      lines = lines.Select(line => line.Trim()).Where(line => line.Length > 0).ToList();
+      StoryCounts.Add(lines.Count());
+
+      foreach (var line in lines) {
+        var billboard = Instantiate(BillboardPrefab) as GameObject;
+        var textComponent = billboard.GetComponentInChildren<Text>();
+        textComponent.text = line;
+        billboard.transform.position = pos;
+        Billboards.Add(billboard);
+        pos += new Vector3(0, 0, -ZSpacing);
+      }
+      pos += new Vector3(XSpacing, 0, 0);
+    }
+  }
+}
+
+// Add a button to the editor to sort the river file list by name.
+[CustomEditor(typeof(GameManager))]
+public class GameManagerFileSorter : Editor {
+  public override void OnInspectorGUI () {
+    DrawDefaultInspector();
+    if (GUILayout.Button("Sort by name")) {
+      var currentTarget = (GameManager) target;
+      currentTarget.StoryFiles = currentTarget.StoryFiles.OrderBy(x => x.name).ToList();
+    }
+  }
+
+  int compareNames(TextAsset a, TextAsset b) {
+    return a.name.CompareTo(b.name);
+  }
+}
---
+++ Assets/Scripts/Rangefinder.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO.Ports;
+
+public class Rangefinder : MonoBehaviour
+{
+    public static Rangefinder Instance;
+
+    [HideInInspector] [System.NonSerialized]
+    public float distance_mm;
+
+    [HideInInspector] [System.NonSerialized]
+    public bool IsActive;
+
+    private SerialPort sp = new SerialPort("COM4", 9600);
+    private List<float> buffer = new List<float>();
+
+    void Awake() {
+      Instance = this;
+    }
+
+    void Start() {
+      StartCoroutine("Connect");
+    }
+
+    void Update() {
+        // Connect();
+      IsActive = sp.IsOpen;
+      ReadSerialData();
+    }
+
+    void OnApplicationQuit() {
+        sp.Close();
+    }
+
+    IEnumerator Connect() {
+      while (!sp.IsOpen) {
+        try {
+          sp.Open();
+          sp.ReadTimeout = 1;
+        } catch (System.Exception e) {
+          Debug.Log(e);
+        }
+
+        yield return new WaitForSeconds(2);
+      }
+    }
+
+    void ReadSerialData() {
+      if (!sp.IsOpen) return;
+
+      string data = "";
+      try {
+          data = sp.ReadTo("\x0A");
+      } catch (System.TimeoutException) {
+      } catch (System.Exception e) {
+          Debug.Log("Exception: " + e);
+      }
+      if (data == "") return;
+
+      int range_us;
+      int.TryParse(data, out range_us);
+      if (range_us == 0) return;
+
+      buffer.Add(range_us / 58f);
+      if (buffer.Count() > 10) buffer.RemoveAt(0);
+      distance_mm = buffer.Average();
+    }
+
+}
