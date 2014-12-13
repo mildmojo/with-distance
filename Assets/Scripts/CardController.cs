b9c5ca6a88511a56a84193cff43f9b1a795740c7
mildmojo <theaterfire@yahoo.com>
2014-12-13 03:04:50 -0500

    Adds modern dust particles, better skybox.
    - Better dust through Shuriken particle emitter.
    - Slightly better skybox through a bigger texture. Still banding.
    - Revises placeholder story.
    - Makes it easier to tweak text properties.
    - Adds placeholder debugging text to be built out with keybinds
      for calibration.
    - Adds proper alpha tweening when switching between cards.

---
+++ Assets/Scripts/CardController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Linq;
+
+public class CardController : MonoBehaviour {
+
+  GameManager gameManager;
+
+  void Start () {
+    gameManager = GameManager.Instance;
+  }
+
+  void Update () {
+
+  }
+
+  void CardChange(int[] indices) {
+    var oldCard = gameManager.Cards[indices.First()];
+    var newCard = gameManager.Cards[indices.Last()];
+    var oldCardText = oldCard.GetComponent<TextController>();
+    var newCardText = newCard.GetComponent<TextController>();
+    Debug.Log("Blending!");
+
+    oldCardText.TweenOut();
+    newCardText.TweenIn();
+  }
+}
